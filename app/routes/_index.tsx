6793e91451ec625e3da968d0fe76aafdca3a2c05
Matt Silverlock <matt@eatsleeprepeat.net>
2023-07-20 13:55:34 -0400

    make query dynamic

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,7 +18,11 @@ interface UserRow {
   settings: string;
 }
 
-const USER_QUERY = "SELECT * FROM users ORDER BY created_at DESC LIMIT 20;";
+const USER_QUERY = "SELECT * FROM users WHERE email_address LIKE ? ORDER BY created_at DESC LIMIT 20;";
+
+function generateRandomInt(min: number, max: number) {
+  return Math.floor(min + Math.random()*(max - min + 1))
+}
 
 // Infer the type our data based on the return type of our loader function.
 // Ref: https://jfranciscosousa.com/blog/typing-remix-loaders-with-confidence
@@ -26,11 +30,16 @@ type LoaderData = Awaited<ReturnType<typeof loader>>;
 
 export const loader = async ({ context, params }: LoaderArgs) => {
   let env = context.env as Env;
-  return await env.DB.prepare(USER_QUERY).all<UserRow>();
+  let likeEmail = `'user_${generateRandomInt(1, 9)}%'`
+  return await env.DB.prepare(USER_QUERY).bind(likeEmail).all<UserRow>();
 };
 
 export default function Index() {
   const { results, meta } = useLoaderData<LoaderData>();
+  if (!results) {
+    throw json({error: "no users found"})
+  }
+
   return (
     <div className="container mx-auto">
       <div className="flex flex-col py-8 justify-center items-center">
